1becbad23285831ec68da602807223e33dfe6927
Mateus Carvalho <mateuscarvalho@MacBook-Pro-de-Mateus-2.local>
2023-07-01 08:57:26 -0300

    Last Update 🚀🌑

--- workspace_eclipse/lab_mqtt/src/main/java/br/inatel/labs/labmqtt/client/TemperatureSubscriber.java
+++ workspace_eclipse/lab_mqtt/src/main/java/br/inatel/labs/labmqtt/client/TemperatureSubscriber.java
@@ -12,21 +12,21 @@ public class TemperatureSubscriber
     public static void main(String[] args) throws MqttException
     {
         String subsciberId = UUID.randomUUID().toString();
-        IMqttClient subscriber = new MqttClient(MyConstants.URI_BROKER, subsciberId);
+        try (IMqttClient subscriber = new MqttClient(MyConstants.URI_BROKER, subsciberId)) {
+            MqttConnectOptions options = new MqttConnectOptions();
+            options.setAutomaticReconnect(true);
+            options.setCleanSession(true);
+            options.setConnectionTimeout(10);
+            subscriber.connect(options);
 
-        MqttConnectOptions options = new MqttConnectOptions();
-        options.setAutomaticReconnect(true);
-        options.setCleanSession(true);
-        options.setConnectionTimeout(10);
-        subscriber.connect(options);
+            subscriber.subscribe(MyConstants.TOPIC_1,
+                    (topic, msg) -> {
+                        byte[] payload = msg.getPayload();
+                        System.out.println("Received payload: " + payload);
+                        System.out.println("Received topic: " + topic);
+                    });
 
-        subscriber.subscribe(MyConstants.TOPIC_1,
-                (topic, msg) -> {
-                    byte[] payload = msg.getPayload();
-                    System.out.println("Received payload: " + payload);
-                    System.out.println("Received topic: " + topic);
-                });
-
-        subscriber.disconnect();
+            subscriber.disconnect();
+        }
     }
 }
--- workspace_eclipse/lab_rest_client/src/test/java/br/inatel/labs/labrest_client/ProdutoControllerTest.java
+++ workspace_eclipse/lab_rest_client/src/test/java/br/inatel/labs/labrest_client/ProdutoControllerTest.java
@@ -3,12 +3,14 @@ package br.inatel.labs.labrest_client;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.math.BigDecimal;
+
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.web.reactive.server.WebTestClient;
 
-import br.inatel.labs.labrest_client.model.dto.ProdutoDTO;
+import br.inatel.labs.labrest.client.model.dto.ProdutoDTO;
 
 @SpringBootTest
 class ProdutoControllerTest {
